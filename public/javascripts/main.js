b9178d9d032109daa19f9f66b61956df2da1edb5
Chris Cat <quanct@gmail.com>
2015-03-09 17:48:09 -0700

    Stylin'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,44 @@
-window.onload = function() {
-  var textarea = document.getElementById('question');
-  var submitBtn = document.getElementById('btn-submit');
+$(function() {
+  var $textarea = $('#question');
+  var $submitBtn = $('#btn-submit');
+  var $waitMarker = $('#wait-marker');
+  var $answerContainer = $('#answer-container');
   var $answer = $('#answer');
 
   var socket = io();
-  var socketId = null;
-
-  socket.on('socketId', function(id) {
-    console.log('socketId: ' + id);
-    socketId = id;
-  });
 
   socket.on('answer', function(answer) {
     $answer.text(answer);
-    $answer.show();
+
+    $waitMarker.fadeTo('slow', 0).hide();
+    $answerContainer.show().fadeTo('slow', 1);
   });
 
-  submitBtn.onclick = function(event) {
-    socket.emit('question', textarea.value);
-  };
+  $submitBtn.on('click', function(event) {
+    var opts = {
+      lines: 12, // The number of lines to draw
+      length: 15, // The length of each line
+      width: 8, // The line thickness
+      radius: 20, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#fff', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '50%', // Top position relative to parent
+      left: '50%' // Left position relative to parent
+    };
+    var spinner = new Spinner(opts).spin();
 
-};
+    socket.emit('question', $textarea.val());
+
+    $answerContainer.fadeTo('slow', 0).hide();
+    $waitMarker.append(spinner.el);
+    $waitMarker.show().fadeTo('slow', 1);
+  });
+});
